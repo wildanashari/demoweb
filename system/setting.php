eedf3ceab74e55af7dc58c10ee4add5d0f2b8e84
wildanashari <102495451+wildanashari@users.noreply.github.com>
2023-04-13 08:03:52 +0700

    Add files via upload

---
+++ system/setting.php
@@ -0,0 +1,12 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+$yearNow = date('Y');
+
+$title = 'First Ultimate Quality X-Suit Release';
+$description = 'Iridescence X-Suit & Golden Pharaoh X-Suit (7-Star)';
+$copyright = 'PUBG MOBILE';
+$theme = '#000';
+$image = 'https://www.pubgmobile.com/images/event/Iridescencex_suit/share/en.jpg';
+$icon = 'https://www.pubgmobile.com/images/event/home/pubg_icon.png';
+?>
